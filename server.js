cd996329f9b683d7308ac5b4ecf1be9a837e1ade
jsnkrm <m.jsnkr@gmail.com>
2019-07-04 11:13:11 +0530

    started using git for chat app

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express')
+
+var bodyParser = require('body-parser')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var dbURL = 'mongodb+srv://user:user@cluster0-hxlos.mongodb.net/test?retryWrites=true&w=majority'
+
+var Message = mongoose.model('Message', {
+    name : String,
+    message : String
+})
+
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) => {
+    res.send(messages)
+    })
+});
+
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+    Message.find({name: user}, (err, messages) => {
+    res.send(messages)
+    })
+});
+
+app.post('/messages', async (req, res) => {
+
+    try {
+        
+        var message = new Message(req.body)
+
+        var savedMesssage = await message.save()
+
+        console.log('message saved');
+
+        var censored = await Message.findOne({"message" : "bad"})
+
+        if(censored) 
+            await Message.findOneAndDelete({_id:censored.id})
+                
+        else
+            io.emit('message', req.body)
+
+        res.sendStatus(200);
+        
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    }   
+    
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+})
+
+mongoose.connect(dbURL, {useNewUrlParser:true} , (err) => {
+    console.log('mongo db connection', err);
+    
+})
+
+var server = http.listen(3000,() => {
+    console.log('app serving on port', server.address().port);
+    
+});
